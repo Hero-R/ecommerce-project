17253a99f27b8b14e0bd91b7f761187533256c41
Hero <ghalou.reda@gmail.com>
2023-02-22 20:03:48 +0100

    Step 13: Pagination
    - Selecting Page Size.

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -98,4 +98,10 @@ export class ProductListComponent implements OnInit {
     )
   }
 
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
 }
